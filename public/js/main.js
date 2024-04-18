298b60b058551ce0dd797a164a9e02e3129dfda6
Reda Ibenahmed <119446257+IBENAHMED@users.noreply.github.com>
2024-04-18 20:02:10 +0100

    Delete public directory

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-const icon = document.querySelector("#burger");
-const active = document.querySelector(".active");
-
-icon.addEventListener('click', (e) => {
-    active.classList.toggle('hidden')
-})
